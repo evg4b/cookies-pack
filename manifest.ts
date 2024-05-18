e1b27d4520b1f80a9b14b676a27cfe7f5f93e69f
Yauheni Abramovich <yauheni.abramovich@appcast.io>
2024-05-18 10:52:52 -0300

    Added extension key

--- manifest.ts
+++ manifest.ts
@@ -18,6 +18,13 @@ export const manifest: ManifestV3Export = {
   name: debug ? `DEV: ${ pkg.displayName }` : pkg.displayName,
   version: pkg.version,
   description: pkg.description,
+  key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuoZ6iyeKFISXJhEau79T' +
+    'Qd/aBiTsPO6RTt6ZAy2lm1px8pDK7PZKBNA5B8CrSa67OJyZvm8HDTVGc4xxXQ41' +
+    'chG0e8ONxYi3k3QRio4u1m3H3S/5xJZtvgEgSbqOLQutdOrYLW9R3ile+AhIQZhR' +
+    'rNL2ni76GOFithkYPdwpn13xj3p7jFVdkYRDyQl25uzCy2dBUk6i9FzgCT/tCvaC' +
+    'cK+TqQkdk0xjeYiGKN/9uucBCew98zE93mEESFOtAOzs6KjYkzo+L7rTlfSeKL/k' +
+    'uErnQ9r/TLZ6HLvj5ZICMmYFEm1hi7bMAUbxp9Fwq1Zew+Ozqrj3Svi4l/Iypa2F' +
+    'vQIDAQAB',
   homepage_url: 'https://github.com/evg4b/cookies-pack',
   options_ui: {
     page: 'src/options/index.html',
