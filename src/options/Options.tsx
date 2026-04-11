e12773301ffa547ed4aae29a67e617624ce88ea9
Evgeny Abramovich <evg.abramovitch@gmail.com>
2026-04-11 17:53:50 -0400

    Cleanup UI

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -8,7 +8,6 @@ const Options: FC = () => {
   return (
     <div className="bg-background text-foreground p-6">
       <div className="flex flex-col gap-6 max-w-sm">
-        <h1 className="text-base font-semibold">Default Settings</h1>
         <div className="flex flex-col gap-4">
           <Switch
             isSelected={settings.clearExistingCookiesFirst}
--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,7 +1,7 @@
 import Options from '@src/options/Options';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './options.css';
+import '../styles.css';
 
 createRoot(document.body).render(
   <React.StrictMode>
