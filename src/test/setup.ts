c0666c549c6dc16aa6b59ca31b815ea788814135
Evgeny Abramovich <evg.abramovitch@gmail.com>
2026-07-11 13:53:18 -0400

    Polifils

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,24 +1,6 @@
 import '@testing-library/jest-dom/vitest';
+import { matchMedia } from 'mock-match-media';
+import ResizeObserverPolyfill from 'resize-observer-polyfill';
 
-if (typeof window.matchMedia !== 'function') {
-  window.matchMedia = (query: string): MediaQueryList => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: () => undefined,
-    removeListener: () => undefined,
-    addEventListener: () => undefined,
-    removeEventListener: () => undefined,
-    dispatchEvent: () => false,
-  });
-}
-
-if (typeof window.ResizeObserver !== 'function') {
-  class ResizeObserverStub {
-    observe(): void { /* noop */ }
-    unobserve(): void { /* noop */ }
-    disconnect(): void { /* noop */ }
-  }
-
-  window.ResizeObserver = ResizeObserverStub;
-}
+window.matchMedia = matchMedia;
+window.ResizeObserver = ResizeObserverPolyfill;
